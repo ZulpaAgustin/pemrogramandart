f03d7398fcd3cdd567e974142d00db66c02c433a
Zulpa Agustin <113565616+ZulpaAgustin@users.noreply.github.com>
2023-09-12 14:24:35 +0700

    Add files via upload

---
+++ test.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('Hello Dart');
+  var nama = "Zulpa";
+  var kelas = "11 PPLG 4";
+  String jk = "Perempuan";
+  int no = 081234578;
+
+  print("Nama : $nama");
+  print("Kelas : $kelas");
+  print("Jenis Kelamin : $jk");
+  print("No HP : $no");
+}
---
+++ tipe.dart
@@ -0,0 +1,15 @@
+void main() {
+  var name = 'hai';
+  final nama = 'hoi';
+  const data = 'hallo';
+  late var age = gret();
+  print(age);
+
+  print(nama);
+  print(name);
+  print(data);
+}
+
+String gret() {
+  return '16';
+}
